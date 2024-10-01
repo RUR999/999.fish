beb2f34e4c5c669c32f472ed5ceb9cb3dacb49a2
Riyaz Ull Rabby <riyazullrabby999@gmail.com>
2024-10-01 20:13:09 +0600

    Update 999.sh

--- 999.sh
+++ 999.sh
@@ -4,102 +4,88 @@ g="\033[1;32m"
 c="\033[1;36m"
 b="\033[1;34m"
 n="\033[0m"
-boxq="\033[1;34m[\033[1;32m?\033[1;34m]"
 boxg="\033[1;34m[\033[1;32m+\033[1;34m]"
 boxr="\033[1;34m[\033[1;31m!\033[1;34m]"
 . <(curl -sLo- "https://raw.githubusercontent.com/RUR999/spinner/refs/heads/main/spin.sh")
 
 banner() {
-    echo -e "\n by RUR 999\n\n${n}"
+    echo -en "\n${b}╔═══╗╔═══╗╔═══╗╔═╦╦═╦╗\n║╔═╗║║╔═╗║║╔═╗║║═╬╣═╣╚╗\n║╚═╝║║╚═╝║║╚═╝║║╔╣╠═║║║\n╚══╗║╚══╗║╚══╗║╚╝╚╩═╩╩╝\n╔══╝║╔══╝║╔══╝║ ${g}By RUR 999\n${b}╚═══╝╚═══╝╚═══╝ \n\n${n}"
 }
-
-setfile(){
-    if [ -f $HOME/../usr/etc/motd ]; then
-    rm -rf $HOME/../usr/etc/motd
-    fi
-    if [ -f $HOME/.config/fish/functions/fish_prompt.fish ]; then
-    rm -rf $HOME/.config/fish/functions/fish_prompt.fish
-    touch $HOME/.config/fish/functions/fish_prompt.fish
-    else
-    touch $HOME/.config/fish/functions/fish_prompt.fish
-    fi
-    if [ -f $HOME/.config/fish/config.fish ]; then
-    rm -rf $HOME/.config/fish/config.fish
-    touch $HOME/.config/fish/config.fish
-    else
-    touch $HOME/.config/fish/config.fish
-    fi
-}
-
-termpro(){
-    echo -en "${boxq} ${g}Do You Want To Change Extra Key & Cursor Style? (y/n)${n} "
-    read tp
-    case $tp in
+rurfish(){
+    echo -en "${boxq} ${g}Do You Want To Install Fish Functions? (y/n)${n} "
+    read yn
+    case $yn in
     Y|y)
-    if [ -f $HOME/.termux/termux.properties ]; then
-    rm -rf $HOME/.termux/termux.properties
-    fi
-    (curl https://raw.githubusercontent.com/RUR999/999.fish/refs/heads/main/files/termux.properties -o $HOME/.termux/termux.properties) &> /dev/null & spin
+    echo -e "${boxg} ${g}Installing Fish Functions${n}"
+    (curl https://raw.githubusercontent.com/RUR999/999.fish/refs/heads/main/999.sh -o 999.sh) &> /dev/null & spin
+    bash 999.sh
+    rm -rf 999.sh
     ;;
     N|n) 
     echo ""
+    chsh -s fish
     ;;
     *) echo -e "${boxr} ${r}Wrong Input"
-    termpro
+    rurfish
     ;;
     esac
 }
 
-
-
-setban(){
-    clear;banner
-    echo -en "${boxq} ${g}Enter Bar Name${n}: "
-    read name
-    while true; do
-    echo -en "${boxq} ${g}Enter Banner Name${n}: "
-    read bname
-    if [[ ${#bname} -gt 8 ]]; then
-    echo -e "${boxr} ${r}Enter Banner Name Less Than 8 Characters${n}"
-    continue
-    else
-    break
-    fi
-    done
-    echo "set name \"${name}\""> $HOME/.config/fish/functions/fish_prompt.fish
-    cat $HOME/.config/fish/functions/temp.fish >> $HOME/.config/fish/functions/fish_prompt.fish
-    echo "set bname \"${bname}\"
-    set name \"${name}\""> $HOME/.config/fish/config.fish
-    cat $HOME/.config/fish/temp2.fish >> $HOME/.config/fish/config.fish
-}
-
-main(){
-    clear; banner
-    setfile
-    setban
-    termpro
-    chsh -s fish
-    termux-reload-settings
-    clear; banner
-    echo -e "${boxg} ${g}Now Exit From Termux And Again Open Termux${n}"
-}
-
+termux-setup-storage
 clear;banner
-pkgs=( fish figlet )
+(pkg update;pkg upgrade -y ) &> /dev/null & spin
+
+pkgs=( x11-repo termux-x11-nightly xfce4 xfce4-goodies tur-repo xcompmgr xmlstarlet )
 for pkg in "${pkgs[@]}";do
     echo -e "${boxg} ${g}Installing ${pkg}${n}"
     (pkg install -y ${pkg}) &> /dev/null & spin
-    if [[ $(command -v ${pkg}) ]]; then
-    echo -e "${boxg}${g} ${pkg} Installed Successfull${n}"
-    else
-    echo -e "${boxr} ${r}${pkg} Install Error ${g}First ${pkg} Install Manually Then Run Again${n}"
-    exit
-    fi
 done
-echo -e "${boxg} ${g}Downloading Functions File${n}"
-(curl https://raw.githubusercontent.com/RUR999/999.fish/refs/heads/main/functions/fish_greeting.fish -o $HOME/.config/fish/functions/fish_greeting.fish) &> /dev/null & spin
-(curl https://raw.githubusercontent.com/RUR999/999.fish/refs/heads/main/functions/fish_prompt.fish -o $HOME/.config/fish/functions/temp.fish) &> /dev/null & spin
-(curl https://raw.githubusercontent.com/RUR999/999.fish/refs/heads/main/functions/config.fish -o $HOME/.config/fish/temp2.fish) &> /dev/null & spin
-main
-rm -rf $HOME/.config/fish/functions/temp.fish
-rm -rf $HOME/.config/fish/temp2.fish
+
+echo -e "${boxg}${g} Installing Chromium Browser ${n}"
+(pkg install -y chromium) &> /dev/null & spin
+echo -e "${boxg}${g} Installing Firefox Browser ${n}"
+(pkg install -y firefox) &> /dev/null & spin
+echo -e "${boxg}${g} Installing VS Code Editor${n}"
+(pkg install -y code-oss) &> /dev/null & spin
+echo -e "${boxg}${g} Installing Parole Media Player${n}"
+(pkg install -y parole) &> /dev/null & spin
+echo -e "${boxg}${g} Installing Ristretto Image Viewer${n}"
+(pkg install -y ristretto) &> /dev/null & spin
+echo -e "${boxg}${g} Installing inkscape Image Editor${n}"
+(pkg install -y inkscape) &> /dev/null & spin
+rurfish
+
+rm -rf $PREFIX/bin/x11
+rm -rf $PREFIX/bin/stopx11
+
+cat >> $PREFIX/bin/x11 <<- _EOF_
+#/data/data/com.termux/files/usr/bin/bash
+
+# Kill open X11 processes
+pkill -f com.termux.x11
+
+# Prepare termux-x11 session
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
+
+# Launch Termux X11 main activity
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+sleep 1
+
+# Run XFCE4 Desktop
+env DISPLAY=:0 dbus-launch --exit-with-session xfce4-session & > /dev/null 2>&1
+
+exit 0
+_EOF_
+
+cat >> $PREFIX/bin/stopx11 <<- _EOF_
+pkill -f com.termux.x11
+_EOF_
+
+chmod +x $PREFIX/bin/x11
+chmod +x $PREFIX/bin/stopx11
+chsh -s fish
+termux-reload-settings
+sleep 3
+clear;banner
+echo -en "${boxg}${g} Just Type ${c}x11 ${g}For Run Termux-x11\n${boxg} ${g}Type ${c}stopx11 ${g}For Stop Termux-x11${n}\n\n\n"
